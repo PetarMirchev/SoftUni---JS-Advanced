b2a8133d63134676e6b647c3b3c7598587644f40
Petar Mirchev <petar.g.mirchev@gmail.com>
2023-05-20 14:43:18 +0300

    Add files via upload

--- 2ArraysandNestedArrays.js
+++ 2ArraysandNestedArrays.js
@@ -41,18 +41,18 @@
 //3. Sum First Last
 
 
-// function sumFirstLastElementArr(input){
+function sumFirstLastElementArr(input){
 
-//         let x = Number(input[0]);
-//         let k = input.length - 1;
-//         let y = Number(input[k]);
+        let x = Number(input[0]);
+        let k = input.length - 1;
+        let y = Number(input[k]);
 
-//         let result = x + y;
-//         console.log(result);
-// }
+        let result = x + y;
+        console.log(result);
+}
 
-// sumFirstLastElementArr(['20', '30', '40']); //60
-// sumFirstLastElementArr(['5', '10']); //15
+sumFirstLastElementArr(['20', '30', '40']); //60
+sumFirstLastElementArr(['5', '10']); //15
 
 
 //*************************************************************************************************** */
@@ -63,36 +63,36 @@
 //4. Negative / Positive Numbers
 
 
-// function negativePositiveNumbers(input){
+function negativePositiveNumbers(input){
 
-//         let arr = [...input]; //[7, -2, 8, 9]
-//         let result = []; //
+        let arr = [...input]; //[7, -2, 8, 9]
+        let result = []; //
 
-//         //Otherwise, if the current element is a positive number (or 0), use a push method to add
-//         //  the number to the end of the array.
+        //Otherwise, if the current element is a positive number (or 0), use a push method to add
+        //  the number to the end of the array.
 
-//         for (let j = 0; j < arr.length; j++) {
+        for (let j = 0; j < arr.length; j++) {
 
-//                 if (arr[j] < 0){
-//                         result.unshift(arr[j]);
-//                 } else {
-//                         result.push(arr[j]); 
-//                 }        
-//         }
-//         console.log(result.join('\n'));
-// }
+                if (arr[j] < 0){
+                        result.unshift(arr[j]);
+                } else {
+                        result.push(arr[j]); 
+                }        
+        }
+        console.log(result.join('\n'));
+}
 
-// negativePositiveNumbers([7, -2, 8, 9] );
-// // -2
-// // 7
-// // 8
-// // 9
+negativePositiveNumbers([7, -2, 8, 9] );
+// -2
+// 7
+// 8
+// 9
 
-// negativePositiveNumbers([3, -2, 0, -1] );
-// // -1
-// // -2
-// // 3
-// // 0
+negativePositiveNumbers([3, -2, 0, -1] );
+// -1
+// -2
+// 3
+// 0
 
 
 //***************************************************************************************************** */
@@ -101,16 +101,16 @@
 
 //05. Smallest Two Numbers
 
-// function find2SmallNumbers(input){
+function find2SmallNumbers(input){
 
-//         let arr = [...input]; //[30, 15, 50, 5]
+        let arr = [...input]; //[30, 15, 50, 5]
         
-//         arr.sort( (a,b) =>  {return a-b}); //[5, 15, 30, 50]
-//         console.log(arr[0],arr[1]); //// 5 15
-// }
+        arr.sort( (a,b) =>  {return a-b}); //[5, 15, 30, 50]
+        console.log(arr[0],arr[1]); //// 5 15
+}
 
-// find2SmallNumbers([30, 15, 50, 5]); // 5 15
-// find2SmallNumbers([3, 0, 10, 4, 7, 3]); // 0 3
+find2SmallNumbers([30, 15, 50, 5]); // 5 15
+find2SmallNumbers([3, 0, 10, 4, 7, 3]); // 0 3
 
 
 //****************************************************************************************************** */
@@ -142,34 +142,34 @@ biggerHalfOfArr([3, 19, 14, 7, 2, 19, 6]); //[7, 14, 19, 19]
 
 //07. Piece of Pies
 
-// function cutPieceFromStartToEnd(arr, startWord, endWord){
+function cutPieceFromStartToEnd(arr, startWord, endWord){
 
-//         let arr1 = [...arr];
-//         let startIndex = arr1.indexOf(startWord); // 1  -> 'Key Lime Pie'
-//         let endIndex = arr1.indexOf(endWord); // 3  -> 'Lemon Meringue Pie'
+        let arr1 = [...arr];
+        let startIndex = arr1.indexOf(startWord); // 1  -> 'Key Lime Pie'
+        let endIndex = arr1.indexOf(endWord); // 3  -> 'Lemon Meringue Pie'
 
-//         let cutPart = arr1.slice(startIndex, endIndex + 1);
-//         return cutPart;
-// }
+        let cutPart = arr1.slice(startIndex, endIndex + 1);
+        return cutPart;
+}
 
-// console.log(cutPieceFromStartToEnd(['Pumpkin Pie',                //['Key Lime Pie',
-//                         'Key Lime Pie',               //'Cherry Pie',
-//                         'Cherry Pie',                 //'Lemon Meringue Pie']
-//                         'Lemon Meringue Pie',
-//                         'Sugar Cream Pie'],
-//                         'Key Lime Pie',
-//                         'Lemon Meringue Pie'));
+console.log(cutPieceFromStartToEnd(['Pumpkin Pie',                //['Key Lime Pie',
+                        'Key Lime Pie',               //'Cherry Pie',
+                        'Cherry Pie',                 //'Lemon Meringue Pie']
+                        'Lemon Meringue Pie',
+                        'Sugar Cream Pie'],
+                        'Key Lime Pie',
+                        'Lemon Meringue Pie'));
 
 
-// console.log(cutPieceFromStartToEnd(
-//         ['Apple Crisp',                  //['Pot Pie',
-//         'Mississippi Mud Pie',           // 'Steak and Cheese Pie',
-//         'Pot Pie',                       // 'Butter Chicken Pie',
-//         'Steak and Cheese Pie',          //'Smoked Fish Pie']
-//         'Butter Chicken Pie',
-//         'Smoked Fish Pie'],
-//         'Pot Pie',
-//         'Smoked Fish Pie'));
+console.log(cutPieceFromStartToEnd(
+        ['Apple Crisp',                  //['Pot Pie',
+        'Mississippi Mud Pie',           // 'Steak and Cheese Pie',
+        'Pot Pie',                       // 'Butter Chicken Pie',
+        'Steak and Cheese Pie',          //'Smoked Fish Pie']
+        'Butter Chicken Pie',
+        'Smoked Fish Pie'],
+        'Pot Pie',
+        'Smoked Fish Pie'));
 
 
 //****************************************************************************************************** */
@@ -185,26 +185,26 @@ biggerHalfOfArr([3, 19, 14, 7, 2, 19, 6]); //[7, 14, 19, 19]
 // The input comes as an array of number elements.
 
 
-// function oddPositionsNumbers(array1) {
+function oddPositionsNumbers(array1) {
 
-//         let arr = [...array1];
-//         let buffer = [];  //[10, 15, 20, 25]
+        let arr = [...array1];
+        let buffer = [];  //[10, 15, 20, 25]
 
-//         for (let i = 0; i < arr.length; i++) {
-//                 let element = arr[i];
-//                 if (i % 2 !== 0){
-//                         buffer.push(element);  
-//                 }
-//         }
-//         //console.log(buffer); //[15, 25]
-//         let increaseArrayBy2 = buffer.map(x => x * 2); //[30, 50]
-//         let result = increaseArrayBy2.reverse().join(" ");
-//         console.log(result);     
-// }
+        for (let i = 0; i < arr.length; i++) {
+                let element = arr[i];
+                if (i % 2 !== 0){
+                        buffer.push(element);  
+                }
+        }
+        //console.log(buffer); //[15, 25]
+        let increaseArrayBy2 = buffer.map(x => x * 2); //[30, 50]
+        let result = increaseArrayBy2.reverse().join(" ");
+        console.log(result);     
+}
 
 
-// oddPositionsNumbers([10, 15, 20, 25]);  // 50 30 
-// oddPositionsNumbers([3, 0, 10, 4, 7, 3]); //6 8 0
+oddPositionsNumbers([10, 15, 20, 25]);  // 50 30 
+oddPositionsNumbers([3, 0, 10, 4, 7, 3]); //6 8 0
 
 
 
@@ -219,31 +219,32 @@ biggerHalfOfArr([3, 19, 14, 7, 2, 19, 6]); //[7, 14, 19, 19]
 
 //Nested Arrays --> 9. Biggest Element
 
+// finds the biggest element in array of arrays
 
 
-// function matrixInMatrixFindBigNumber(input){
+function matrixInMatrixFindBigNumber(input){
 
-//         let maxNumber = Number.MIN_SAFE_INTEGER;
+        let maxNumber = Number.MIN_SAFE_INTEGER;
 
-//         for (let i = 0; i < input.length; i++) {
-//                 let el = input[i];
-//                 //console.log(el);
-//                 for (let j = 0; j < el.length; j++) {
-//                         let element = el[j];
-//                         //console.log(element);
+        for (let i = 0; i < input.length; i++) {
+                let el = input[i];
+                //console.log(el);
+                for (let j = 0; j < el.length; j++) {
+                        let element = el[j];
+                        //console.log(element);
 
-//                         if (element > maxNumber){
-//                                  maxNumber = element;  
-//                         }
-//                 }
-//         }
+                        if (element > maxNumber){
+                                 maxNumber = element;  
+                        }
+                }
+        }
 
-//         console.log(maxNumber);
-// }
+        console.log(maxNumber);
+}
 
-// matrixInMatrixFindBigNumber([[20, 50, 10],[8, 33, 145]]); // 145 
-// console.log(`-----------------------------`);
-// matrixInMatrixFindBigNumber([[3, 5, 7, 12],[-1, 4, 33, 2],[8, 3, 0, 4]]) // 33
+matrixInMatrixFindBigNumber([[20, 50, 10],[8, 33, 145]]); // 145 
+console.log(`-----------------------------`);
+matrixInMatrixFindBigNumber([[3, 5, 7, 12],[-1, 4, 33, 2],[8, 3, 0, 4]]) // 33
 
 
 
@@ -254,4 +255,88 @@ biggerHalfOfArr([3, 19, 14, 7, 2, 19, 6]); //[7, 14, 19, 19]
 
 //Nested Arrays --> 10. Diagonal Sums
 
+// A square matrix of numbers comes as an array of arrays, each array holding numbers. 
+// Write a function that finds the sum at the main and the secondary diagonals.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is printed on the console, on a single line separated by space. 
+// First print the sum at the main diagonal, then the sum at the secondary diagonal.
 
+
+function matrixDiagonalSum(input){
+
+        let firstDiagonalSum = 0;
+        let secondaryDiagonalSum = 0;
+        let firstIndex = 0;
+        let secondaryIndex = input[0].length - 1;
+
+        // let arr1 = input[0]; // [3, 5, 17]
+        // let lastElementArr = input.length-1; // get last nested array element
+        // let arr2 = input[lastElementArr]; // [1, -8, 89]
+
+
+        // for (let i = 0; i < input.length; i++) { //furst array elements
+        //         let element = input[i][i]; // 3 7 89
+        //         console.log(element);
+        // }
+
+        input.forEach(array => {
+                firstDiagonalSum += array[firstIndex++];
+                secondaryDiagonalSum += array[secondaryIndex--];
+        });
+        console.log(firstDiagonalSum + ' ' + secondaryDiagonalSum);      
+}
+
+
+
+matrixDiagonalSum([[20, 40], [10, 60]]);  // 80 50 
+console.log(`-------------------------------------------`);
+matrixDiagonalSum([[3, 5, 17],[-1, 7, 14],[1, -8, 89]]); // 99 25
+
+
+//******************************************************************************************************* */
+
+
+
+
+
+//11. Equal Neighbors
+
+
+// Write a function that finds the number of equal neighbor pairs inside a matrix of 
+// variable size and type (numbers or strings).
+// The input comes as an array of arrays, containing string elements (2D matrix of strings).
+
+
+
+function equalNeighbors(matrix){
+        
+        let pairCount = 0; 
+        for (let i = 0; i < matrix.length; i++) {
+                let innerArr = matrix[i];
+
+                for (let j = 0; j < innerArr.length; j++) {
+                        // let element = innerArr[j];
+                        
+                    if (i - 1 >=0 && matrix[i][j] == matrix[i - 1][j]){
+                        pairCount++;
+                    }
+
+                    if (j - 1 >=0 && matrix[i][j] == matrix[i][j - 1]){
+                        pairCount++;
+                    }             
+                }         
+        }
+        console.log(pairCount);
+}
+
+
+equalNeighbors([['2', '3', '4', '7', '0'],
+                ['4', '0', '5', '3', '4'],
+                ['2', '3', '5', '4', '2'],
+                ['9', '8', '7', '5', '4']]);   // 1
+
+console.log(`---------------------------`);
+
+equalNeighbors([['test', 'yes', 'yo', 'ho'],
+                ['well', 'done', 'yo', '6'],
+                ['not', 'done', 'yet', '5']]); //2
---
+++ 2ExerciseArraysAndNestedArrays.js
@@ -0,0 +1,367 @@
+
+                        // 2 UPR - Exercise: Arrays and Nested Arrays
+
+
+
+
+// 1. Print an Array with a Given Delimiter
+
+
+//The input comes as two parameters – an array of strings and a string. 
+//The second parameter is the delimiter.
+
+
+// function joinArrayWithDelimiter(array, delimiter) {
+
+//     let newArray = array.join(delimiter);
+
+//     console.log(newArray);
+// }
+
+// joinArrayWithDelimiter(['One','Two','Three','Four','Five'],'-');
+// //One-Two-Three-Four-Five
+// joinArrayWithDelimiter(['How about no?','I','will','not','do','it!'],'_');
+// //How about no?_I_will_not_do_it!
+
+
+//**************************************************************************************** */
+
+
+//02. Print every N-th Element from an Array
+
+
+// The input comes as two parameters – an array of strings and a number. 
+// The second parameter is N – the step.
+// The output is every element on the N-th step starting from the first one. 
+// If the step is 3, you need to return the 1-st,
+// the 4-th, the 7-th … and so on, until you reach the end of the array. 
+
+// function getEveryN_Element(array, step){
+
+//     let arr = [...array];
+//     let number = Number(step);
+//     let resultArray = [];
+
+//     for (let i = 0; i < array.length; i+=number) {
+//         let element = array[i]; // 5 31 20
+//         //console.log(element);
+//         resultArray.push(element);
+//     }
+//     return resultArray;
+// }
+
+
+// getEveryN_Element(['5','20','31','4','20'],2);  //['5', '31','20']
+// console.log(`--------------------------------`);
+// getEveryN_Element(['dsa','asd','test','tset'],2); // ['dsa', 'test']
+// console.log(`--------------------------------`);
+// getEveryN_Element(['1','2','3','4','5'],6); // ['1']
+
+
+//****************************************************************************************************** */
+
+
+
+
+//03. Add and Remove Elements 
+
+
+// function addAndRemoveElements(commandsInArray){
+
+//     let resultArray = [];
+//     let count = 0;
+
+//     for (let i = 0; i < commandsInArray.length; i++) {
+//         let element = commandsInArray[i];
+//         count++;
+
+//         if (element === 'add') {
+//             resultArray.push(count); 
+//         }
+//         if (element === 'remove'){
+//             resultArray.pop();
+//         }
+//     }
+
+//     if (resultArray.length === 0 ){
+//         console.log('Empty');
+//     } else {
+
+//         for (let j = 0; j < resultArray.length; j++) {
+//             let element = resultArray[j];
+//             console.log(element);
+//         }
+//     }
+// }
+
+// addAndRemoveElements(['add','add','add','add']); //1 2 3 4
+// console.log(`------------------------------------------`);
+// addAndRemoveElements(['add','add','remove','add','add']); //1 4 5
+// console.log(`------------------------------------------`);
+// addAndRemoveElements(['remove','remove','remove']); // Empty
+
+
+//********************************************************************************************************* */
+
+
+
+
+//4. Rotate Array
+
+// The array should be rotated to the right side, meaning that the last
+// element should become the first, upon rotation. 
+
+// function rotateArray(arr, amountRotation){
+
+//     let count = amountRotation;
+//     let resultArray = [...arr];
+
+//     for (let i = 0; i < count; i++) {
+      
+//        let cutElement = resultArray.pop(); // 4 , 3
+//        //console.log(cutElement);
+        
+//        resultArray.unshift(cutElement);
+//     }
+//     console.log(resultArray.join(' '));
+// }
+
+
+// rotateArray(['1','2','3','4'],2);  //3 4 1 2 
+// console.log(`--------------------------------`);
+// rotateArray(['Banana','Orange','Coconut','Apple'],15); // Orange Coconut Apple Banana
+
+
+
+//********************************************************************************************************* */
+
+
+
+
+// 5. Extract Increasing Subset from Array
+
+
+// function filterOnlyBiggest(array){
+
+//     let arr = [...array];
+//     let buffArray = [];
+//     let x = Number.MIN_SAFE_INTEGER;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let numberX = Number(arr[i]);
+
+//         if (numberX >= x){
+//             buffArray.push(numberX);
+//             x = numberX;
+//         }   
+//     }
+//     //console.log(buffArray);
+//     return buffArray;
+
+
+//             // // alternative to the top whit REDUCE
+//             // let biggest = Number.MIN_SAFE_INTEGER;
+
+//             // const output = arr.reduce((acc, curr) => {
+//             //     if (curr >= biggest) {
+//             //     biggest = curr;
+
+//             //     const tempArr = acc;
+//             //     tempArr.push(curr);
+//             //     return tempArr;
+//             //     }
+
+//             //     return acc;
+//             // }, []);
+
+//             // return output;
+// }
+
+// filterOnlyBiggest([1,3,8,4,10,12,3,2,24]); // [1, 3, 8, 10, 12, 24]
+// console.log(`-----------------------`);
+// filterOnlyBiggest([1,2,3,4]); // [1, 2, 3, 4]
+// console.log(`-----------------------`);
+// filterOnlyBiggest([20,3,2,15,6,1]); // [20]
+
+
+//******************************************************************************************************** */
+
+
+
+// 06. List Of Names
+
+
+// You will receive an array of names. Sort them alphabetically in ascending order and print a 
+// numbered list of all the names, each on a new line.
+
+// function sortListNames(arr){
+
+//     let array = [...arr];
+
+//     array.sort( (a,b) => a.localeCompare(b)); //['Bob', 'Christina', 'Ema', 'John']
+
+//     let count = 1;
+
+//     for (let i = 0; i < array.length; i++) {
+//         let element = array[i];
+//         console.log(`${count}.${element}`);
+//         count++;
+//     }
+// }
+
+
+// sortListNames(["John","Bob","Christina","Ema"]);
+// // 1.Bob
+// // 2.Christina
+// // 3.Ema
+// // 4.John
+
+
+//******************************************************************************************************* */
+
+
+
+// 07. Sorting Numbers
+
+// function that sorts an array of numbers so that the first element is the smallest one, the second is the
+// biggest one, the third is the second smallest one, the fourth is the second biggest one, and so on.
+// Return the resulting array.
+
+// function sortNumbers(arr){
+
+//     let array = [...arr];
+//     let buffArr =[];
+//     array.sort( (a,b) => a-b); //(10) [-3, 1, 3, 18, 31, 48, 52, 56, 63, 65]
+
+//     while (array.length !== 0) {
+//         let x  = array.shift()
+//         let y = array.pop()
+//         buffArr.push(x,y); //-3, 65
+//     }
+
+//     //console.log(buffArr); //(10) [-3, 65, 1, 63, 3, 56, 18, 52, 31, 48]
+//     return buffArr;
+// }
+
+// sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]); // [-3, 65, 1, 63, 3, 56, 18, 52, 31, 48]
+
+
+//************************************************************************************************************ */
+
+ 
+// 8. Sort an Array by 2 Criteria   !!!!!!!!!!!!!!!!!!!!
+
+
+// Write a function that orders a given array of strings, by a length in ascending order as primary criteria, 
+// and by alphabetical value in ascending order as second criteria. The comparison should be case-insensitive.
+// The input comes as an array of strings.
+
+// function sortArrBy2Criteria(arr){
+
+//     let array = [...arr];
+
+//     let result = array.sort( (a,b) => {
+//         let criteria1 = a.length - b.length ;// word length
+//         let criteria2 = a.localeCompare(b); // by alphabetical
+//         return criteria1 || criteria2
+//     });
+
+//     //console.log(result); //['beta', 'alpha', 'gamma']
+
+//     //print logic
+//     for (let i = 0; i < result.length; i++) {
+//         let element = result[i];
+//         console.log(element);
+//     }
+// }
+
+
+// sortArrBy2Criteria(['alpha','beta', 'gamma']);
+// // beta
+// // alpha
+// // gamma
+// console.log(`----------------------------`);
+// sortArrBy2Criteria(['Isacc','Theodor', 'Jack','Harrison','George']);
+// // Jack
+// // Isacc
+// // George
+// // Theodor
+// // Harrison
+// console.log(`----------------------------`);
+// sortArrBy2Criteria(['test','Deny', 'omen','Default']);
+// // Deny
+// // omen
+// // test
+// // Default
+
+
+//************************************************************************************************************ */
+
+
+
+// Multidimensional Arrays (Matrices)
+
+
+// 9. Magic Matrices
+
+// function that checks if a given matrix of numbers is magical. A matrix is magical if the sums 
+// of the cells of every row and every column are equal.
+// The input comes as an array of arrays, containing numbers (number 2D matrix). 
+// The input numbers will always be positive.
+
+
+function mMatrix(arr){
+    let matrix = [...arr];
+    let arr1 = matrix[0];
+    let refSumRow = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let element = arr1[i];
+        refSumRow += element;
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        let sumRow = 0;
+        let sumCol = 0;
+    
+        for (let col = 0; col < matrix.length; col++) {
+          sumRow += matrix[col][row];
+          sumCol += matrix[row][col];
+        }
+    
+        if (refSumRow !== sumRow || refSumRow !== sumCol) {
+          //console.log(false);
+          return false;
+        }
+      }
+      //console.log(true);
+      return true;
+}
+
+mMatrix([[4, 5, 6],
+        [6, 5, 4],
+        [5, 5, 5]]);  //true
+console.log(`------------------------------------`);
+mMatrix([[11, 32, 45],
+        [21, 0, 1],
+        [21, 1, 1]]); // false 
+console.log(`------------------------------------`);
+mMatrix([[1, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0]]); // true
+
+
+
+//******************************************************************************************************* */
+
+
+// 10. Tic-Tac-Toe
+
+
+//11. Diagonal Attack
+
+
+// 12. *Orbit
+
+
+//13. Spiral Matrix
